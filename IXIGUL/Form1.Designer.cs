3c79534c9747f4ef383e5f6fad0ccac403408e0b
u101040 <u101040@deshalit.local>
2019-09-19 15:48:57 +0300

    Add project files.

---
+++ IXIGUL/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace IXIGUL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            
+            
+            for (int i = 1; i <= 3; i++)
+            {
+                for (int j = 1; j <= 3; j++)
+                {
+                    System.Windows.Forms.Button button = new System.Windows.Forms.Button();
+                    this.SuspendLayout();
+                    // 
+                    // button1
+                    // 
+                    button.Location = new System.Drawing.Point(50 + (i-1)*100, 50 + (j-1) * 100);
+                    button.Name = "button"+(i+j).ToString();
+                    button.Size = new System.Drawing.Size(100, 100);
+                    button.TabIndex = 0;
+                    button.Text = "";
+                    button.UseVisualStyleBackColor = true;
+                    button.Click += new System.EventHandler(this.Button_Click);
+                    this.Controls.Add(button);
+                    this.buttons[i-1, j-1] = button;
+                }
+            }
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 400);
+            
+            this.Name = "Form1";
+            this.Text = "X - 0 - X";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button[,] buttons = new System.Windows.Forms.Button[3, 3];
+    }
+}
+
---
+++ IXIGUL/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IXIGUL
+{
+    public partial class Form1 : Form
+    {
+        private char turn = 'X';
+        private int turn_Num = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Button_Click(object sender,EventArgs e)
+        {
+            if (sender is Button && ((Button)sender).Text == "")
+            {
+                ((Button)sender).Text = this.turn.ToString();
+                if (this.turn == 'X')
+                    this.turn = '0';
+                else
+                    this.turn = 'X';
+
+            }
+
+            string winner = this.CheckForWin();
+            if (winner != "")
+            {
+                this.Text = winner;
+                if (Text == "Draw")
+                {
+                    MessageBox.Show(Text);
+                }
+                else
+                {
+                    MessageBox.Show(Text + " Has Won!");
+                }
+                
+            }
+        }
+
+        public string CheckForWin()
+        {
+            this.turn_Num++;
+            
+            for (int i = 0; i < 3; i++)
+            {
+                if (this.buttons[i, 0].Text == this.buttons[i, 1].Text && this.buttons[i, 1].Text == this.buttons[i, 2].Text)
+                    return this.buttons[i, 0].Text;
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (this.buttons[0, i].Text == this.buttons[1, i].Text && this.buttons[1, i].Text == this.buttons[2, i].Text)
+                    return this.buttons[1, i].Text;
+            }
+
+            if (this.buttons[0, 0].Text == this.buttons[1, 1].Text && this.buttons[1, 1].Text == this.buttons[2, 2].Text)
+            {
+                return this.buttons[0, 0].Text;
+            }
+
+            if (this.buttons[0, 2].Text == this.buttons[1, 1].Text && this.buttons[1, 1].Text == this.buttons[2, 0].Text)
+            {
+                return this.buttons[0, 0].Text;
+            }
+
+            if (this.turn_Num == 9)
+            {
+                return "Draw";
+            }
+            return "";
+
+        }
+    }
+}
